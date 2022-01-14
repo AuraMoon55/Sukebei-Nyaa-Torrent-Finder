d6b8691c224103a4dfe77172d07ef573da5b3f89
Anshul Garg <garganshul553@gmail.com>
2022-01-14 12:31:01 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,8 +1,10 @@
 from pyrogram import Client
+import os
 
-API_ID = 
-API_HASH = ""
-BOT_TOKEN = ""
+API_ID = int(os.environ.get("API_ID", ""))
+API_HASH = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("TOKEM", "")
+AUTH_CHAT = int(os.environ.get("AUTH_CHAT", ""))
 
 NYAA = Client("Nyaa", api_id = API_ID, api_hash = API_HASH, bot_token = BOT_TOKEN)
 with NYAA:
