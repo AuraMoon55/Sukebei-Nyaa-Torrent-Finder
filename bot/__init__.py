0fb9e21bd3cf7398fcd2dd9195688ad09ae3f6c0
Anshul Garg <garganshul553@gmail.com>
2022-02-07 02:22:32 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -4,7 +4,7 @@ import os
 API_ID = 2919867
 API_HASH = "90dd95178a8d13a69bfdbc7da68d23a4"
 BOT_TOKEN = "5015207309:AAEoifISfnyTYGGZkPLWnXpMQVGM7mEkGf8"
-AUTH_CHAT = -613675532
+AUTH_CHAT = -1001652493076
 
 NYAA = Client("Nyaa", api_id = API_ID, api_hash = API_HASH, bot_token = BOT_TOKEN)
 with NYAA:
